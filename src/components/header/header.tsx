f0028e69a6c720cfcf9171bf937a7a46d47afa39
eleonora <nebrudko18@gmail.com>
2024-09-29 15:17:39 +0300

    added header

---
+++ src/components/header/header.tsx
@@ -0,0 +1,50 @@
+import { Menu } from '@mui/icons-material'
+import { AppBar,  Button, IconButton, LinearProgress, Toolbar, Typography } from '@mui/material'
+import { useCallback } from 'react'
+import { useSelector } from 'react-redux'
+import { NavLink } from 'react-router-dom'
+import { selectAppStatus } from 'services/reducers/appSlice'
+import { authThunks, selectIsLoggedIn } from 'services/reducers/authSlice'
+import { useActions } from 'common/hooks'
+import { FlexContainer } from 'components/flex-container'
+
+type Props = {
+  theme: string
+  toggleTheme: () => void
+}
+
+export const Header = ({ theme, toggleTheme }: Props) => {
+  let status = useSelector(selectAppStatus)
+  let isLoggedIn = useSelector(selectIsLoggedIn) //не залогинены
+  const { logOutTC } = useActions(authThunks)
+
+  const logOutHandler = useCallback(() => {
+    logOutTC()
+  }, [logOutTC])
+
+  return (
+    <AppBar position="static" sx={{ borderRadius: '5px' }}>
+      <Toolbar variant="dense" sx={{ display: 'flex', justifyContent: 'space-between' }}>
+        <FlexContainer>
+          <IconButton edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
+            <Menu />
+          </IconButton>
+          <Typography variant="h6" color="inherit" component="div">
+            Todolist
+          </Typography>
+        </FlexContainer>
+        <FlexContainer jc='flex-end'>
+          <Button variant="outlined" size="small" color={'inherit'} onClick={toggleTheme} sx={{ mr: '10px' }}>
+            {theme}
+          </Button>
+          {isLoggedIn && (
+            <Button variant="outlined" size="small" color={'inherit'} onClick={logOutHandler} sx={{ mr: '10px' }}>
+              <NavLink to="/login">Log Out</NavLink>
+            </Button>
+          )}
+        </FlexContainer>
+      </Toolbar>
+      {status === 'loading' && <LinearProgress />}
+    </AppBar>
+  )
+}
