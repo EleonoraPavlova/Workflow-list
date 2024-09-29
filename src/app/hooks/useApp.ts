3416fbe05ce91bbe34ba097f4fa1d485c13f08bb
eleonora <nebrudko18@gmail.com>
2024-09-29 16:10:46 +0300

    added app component

---
+++ src/app/hooks/useApp.ts
@@ -0,0 +1,39 @@
+import { CircularProgress, createTheme, styled } from '@mui/material'
+import { blue, purple } from '@mui/material/colors'
+import { appThunks, selectAppInitialized } from 'services/reducers/appSlice'
+import { useActions } from 'common/hooks'
+import { useEffect, useState } from 'react'
+import { useSelector } from 'react-redux'
+
+export function useApp() {
+  const { setAppInitializeTC } = useActions(appThunks)
+  let initialized = useSelector(selectAppInitialized) //first initialization
+
+  let [lightMode, setLightMode] = useState<boolean>(true)
+  let theme = lightMode ? 'dark' : 'light'
+  const themeHandler = createTheme({
+    palette: {
+      primary: blue,
+      secondary: purple,
+      mode: lightMode ? 'light' : 'dark',
+    },
+  })
+
+  const toggleTheme = () => {
+    setLightMode(!lightMode)
+  }
+
+  const CustomCircularProgress = styled(CircularProgress)(({ theme }) => ({
+    '& circle': {
+      strokeWidth: 2,
+    },
+  }))
+
+  useEffect(() => {
+    if (!initialized) {
+      setAppInitializeTC()
+    }
+  }, [])
+
+  return { lightMode, themeHandler, theme, CustomCircularProgress, toggleTheme }
+}
