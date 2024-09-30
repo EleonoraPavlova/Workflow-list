e6322bf21dc28eca9eab375ff02711e8973572a0
eleonora <nebrudko18@gmail.com>
2024-09-30 10:31:28 +0300

    added gh-pages

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import ReactDOM from 'react-dom/client'
 import reportWebVitals from './reportWebVitals'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import { store } from 'services/store'
 import { App } from 'app'
@@ -9,9 +9,9 @@ import './styles/index.scss'
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
   <Provider store={store}>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </Provider>
 )
 
